dc75a92d2c141600fc774f6905b43ac7fac6f390
grit-li <grit.li@qq.com>
2024-09-21 01:51:00 -0400

    [BOOT更新]boot增加env.ini，并打包固件中

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ APP_BUILD_DATE=$(date --date="+12 hours" +%Y%m%d)
 APP_PACKET_NAME=STM32F103ZETx_$APP_BUILD_DATE.hex
 
 $ROOT_PATH/boot/stm32_mkimage -A arm -O rtthread -T kernel -C none -a 0x08020040 -e 0x08020041 -n "rtthread app" -d out/bin/app.bin out/app.img
+$ROOT_PATH/boot/stm32_env boot/env.ini out/env.bin
 $ROOT_PATH/boot/stm32_bin2hex 0x08020000 out/app.img out/app.hex
 $ROOT_PATH/boot/stm32_bin2hex 0x08000000 boot/iboot.bin out/iboot.hex
-$ROOT_PATH/boot/stm32_hexmerge out/$APP_PACKET_NAME out/app.hex out/iboot.hex
+$ROOT_PATH/boot/stm32_bin2hex 0x08008000 out/env.bin out/env.hex
+$ROOT_PATH/boot/stm32_hexmerge out/$APP_PACKET_NAME out/app.hex out/env.hex out/iboot.hex 
 cp out/$APP_PACKET_NAME /mnt/hgfs/share
